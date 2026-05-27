2e099abbd78300e21256aeab57a695e8301f0b29
Sebastian Blaszczyk <s.blaszczyk.633@studms.ug.edu.pl>
2026-05-27 13:19:35 +0200

    Added Spring Boot dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "4.0.6"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.github.wheezybaton"
@@ -12,6 +14,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks.test {
