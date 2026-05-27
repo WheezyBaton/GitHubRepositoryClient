40f7598353555fc33ff6a24ce8a1a07c2db4b190
Sebastian Blaszczyk <s.blaszczyk.633@studms.ug.edu.pl>
2026-05-27 14:27:31 +0200

    Renamed main file

---
+++ src/main/java/com/github/wheezybaton/GithubRepositoryClient.java
@@ -0,0 +1,12 @@
+package com.github.wheezybaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepositoryClient.class, args);
+    }
+}
